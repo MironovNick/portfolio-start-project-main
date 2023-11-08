06bec383a1bbe9f223b5ea003e660ad2742876b5
MironovNik <mironov.nik88@gmail.com>
2023-11-08 16:27:45 +0300

    Add header component to App.tsx

---
+++ src/assets/layout/header/Header.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <header>
+            <a>
+                <img src="" alt=""/>
+            </a>
+            <nav>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Skills</a></li>
+                    <li><a href="">Works</a></li>
+                    <li><a href="">Testimony</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
